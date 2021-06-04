f24b49e25ee481fc139e91d0734205452f98bc20
aaliceinw <84022510+aaliceinw@users.noreply.github.com>
2021-06-04 22:44:53 +0100

    Experimented with vertical menus
    removed section
    experimented with different vertical menus

---
+++ demo4js.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px*/
+function openNav () {
+    document.getElementById("mySidenav") .style.width = "250";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySideNav") .style.width ="0";
+}
